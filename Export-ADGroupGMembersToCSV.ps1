3b476280eb92dd6c17ba295f86f6d14e03526d8b
Abdulla Shaheed <evenexzero@gmail.com>
2023-12-18 13:21:55 +0500

    Initial commit with README and AD Group Member Export script
    Added README.md explaining the purpose, usage, and features of the script. Included the PowerShell script AD-Group-Member-Export.ps1 for exporting AD group member details.

---
+++ Export-ADGroupGMembersToCSV.ps1
@@ -0,0 +1,56 @@
+﻿# Get all AD groups
+$groups = Get-ADGroup -Filter *
+
+# Prepare an array to hold all results
+$results = @()
+
+# Initialize counters
+$totalGroups = $groups.Count
+$currentGroup = 0
+$totalRecords = 0
+
+# Get the directory of the script
+$scriptDirectory = $PSScriptRoot
+Write-Host "Script Directory: $scriptDirectory"
+
+# Iterate over each group and get members
+foreach ($group in $groups) {
+    # Update group progress
+    $currentGroup++
+    Write-Progress -Activity "Processing Groups" -Status "Processing Group $currentGroup of $totalGroups" -PercentComplete (($currentGroup / $totalGroups) * 100)
+
+    # Get members who are users
+    $groupMembers = Get-ADGroupMember $group | 
+                    Where-Object {$_.objectclass -eq "user"}
+
+    # Iterate over each user and get their department
+    foreach ($member in $groupMembers) {
+        try {
+            $user = Get-ADUser $member -Property Department
+            $results += [PSCustomObject]@{
+                GroupName = $group.Name
+                Department = $user.Department
+                'Username' = $user.SamAccountName
+                Name = $user.Name
+            }
+            $totalRecords++
+        } catch {
+            Write-Warning "Unable to find user for identity: $member"
+        }
+    }
+}
+
+# Construct CSV file path
+$csvPath = Join-Path -Path $scriptDirectory -ChildPath "ADGroupG_Members_Departments_StaffIDs.csv"
+Write-Host "CSV will be saved at: $csvPath"
+
+# Export results to CSV
+try {
+    $results | Export-Csv -Path $csvPath -NoTypeInformation
+    Write-Host "CSV file saved successfully."
+} catch {
+    Write-Error "Error in saving CSV file: $_"
+}
+
+# Display the final counts
+Write-Host "Processing Complete. Total Groups: $totalGroups, Total Records Processed: $totalRecords"
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# AD-Group-Member-Export
- Active Directory All Groups Member Exporter
+# Active Directory All Groups Member Exporter
+
+This PowerShell script is designed for comprehensive extraction of member information from all Active Directory (AD) groups. It efficiently retrieves details such as department names, usernames, and full names of the group members, exporting this aggregated data into a CSV file.
+
+## Features
+- **Comprehensive Group Processing**: Extracts member information from all AD groups, providing a complete overview.
+- **Detailed Member Insights**: Gathers essential details like department, username, and full name for each group member.
+- **Progress Display**: Includes a progress bar to track the script's execution, enhancing the user experience.
+- **CSV Output**: The data is exported into a CSV file named `ADGroupG_Members_Departments_StaffIDs.csv`, suitable for various administrative and auditing needs.
+
+## Use Case
+This script is ideal for IT administrators, system auditors, and support staff who require an exhaustive list of group memberships across the entire Active Directory. It simplifies data gathering for audits, reporting, or administrative tasks in environments with multiple AD groups.
+
+## How to Use
+1. Ensure you have the necessary AD module loaded and appropriate permissions in your PowerShell session.
+2. Run the script.
+3. The output CSV file will be saved in the script's directory, named `ADGroupG_Members_Departments_StaffIDs.csv`.
+
